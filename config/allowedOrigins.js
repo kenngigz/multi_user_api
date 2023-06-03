260c3f3edd3b1184894e1ac3f89f39dc918548b8
Kenneth Ngige <malkiellov@gmail.com>
2023-06-03 19:38:58 +0300

    corrected a typo in the allowed arigin urls

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000'
-	'https://multi-user-auth.netlify.app'
+  "https://www.yoursite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:3000",
+  "https://multi-user-auth.netlify.app",
 ];
 
 module.exports = allowedOrigins;
